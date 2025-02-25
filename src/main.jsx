b146af1aa94fc433a5e573ef089407f9d497c341
Oudom Phoem <oudomphoem@gmail.com>
2025-02-25 22:12:46 +0700

    fix: conflict with contactPage

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,13 @@ import RootLayout from "./components/layout/RootLayout.jsx";
 import ProgressBar from "./components/progress/ProgressBar.jsx";
 import "./i18n.js";
 import CoursesMain from "./page/courses/CoursesMain.jsx";
-<<<<<<< HEAD
 import Login from "./page/auth/Login.jsx";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
 import Register from "./page/auth/Register.jsx";
 import ForgotPassword from "./page/auth/ForgotPassword.jsx";
 import Dashboard from "./page/dashboard/Dashboard.jsx";
+import Contact from "./page/contact/ContactMain.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -25,6 +25,7 @@ createRoot(document.getElementById("root")).render(
           <Route element={<RootLayout />}>
             <Route path="/" element={<HomeMain />} />
             <Route path="/courses" element={<CoursesMain />} />
+            <Route path="/contact" element={<Contact />} />
           </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
@@ -35,22 +36,5 @@ createRoot(document.getElementById("root")).render(
         </Routes>
       </BrowserRouter>
     </Provider>
-=======
-import Contact from "./page/contact/ContactMain.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ProgressBar />
-    <BrowserRouter>
-      <Routes>
-        <Route element={<RootLayout />}>
-          <Route path="/" element={<HomeMain />} />
-          <Route path="/courses" element={<CoursesMain />} />
-          <Route path="/contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
->>>>>>> contactPage
   </StrictMode>
 );
-
