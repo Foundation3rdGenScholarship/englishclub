3153ed687c8a7ab1d2749abd7e49b8e38366d49d
Leaphea <ahpingpingshu@gmail.com>
2025-03-01 16:35:57 +0700

    add circle for background

--- src/page/home/HomeMain.jsx
+++ src/page/home/HomeMain.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "./Hero";
 import shap from "../../../public/svg/1.svg";
+import ellipseHalf from "../../../public/svg/2.svg";
 
 const HomeMain = () => {
   return (
@@ -10,6 +11,13 @@ const HomeMain = () => {
           src={shap}
           alt=""
           className="absolute mt-[-100px] w-[600px] right-0 z-[-1]"
+        />
+      </div>
+      <div className="relative">
+        <img
+          src={ellipseHalf}
+          alt=""
+          className="absolute mt-[170px] w-[200px] left-0 z-[-1]"
         />
       </div>
 
