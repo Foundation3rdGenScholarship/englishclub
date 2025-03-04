463b7996dd7dc694dcd71108b3f6fe0067ca2c2e
Eric-Va01 <ericva014@gmail.com>
2025-03-04 23:21:30 +0700

    aboutpage push

--- src/page/aboutus/AboutUsShape.jsx
+++ src/page/aboutus/AboutUsShape.jsx
@@ -4,19 +4,17 @@ import vector2 from "../../../public/img/vextor/Vector20.svg";
 
 export default function AboutUsShape({children , className=""}) {
   return (
-    <div {...className}>
+    <div className="max-w-screen-xl" {...className}>
       <div>
-        <img
-          src={vector2}
-          alt=""
-          className="absolute mt-[54px] w-[445px] h-[762px]  right-0 overflow-hidden -z-10"
-        />
+        <img src={vector2} alt="shape"
+          className="absolute top-16 -right-20 -z-10 hidden md:block w-[300px] sm:w-[400px] md:w-[450px] lg:w-[500px] xl:w-[550px] sm:h-[300px] md:h-[550px] lg:h-[600px] xl:h-[700px] "
+          />
       </div>
       <div>
         <img
           src={vector1}
-          alt=""
-          className="absolute mt-[147px] w-[755px] h-[581px] left-0 overflow-hidden -z-10"
+          alt="shape"
+          className="absolute top-32 left-0 -z-10 hidden md:block w-[400px] sm:w-[500px] md:w-[600px] lg:w-[700px] xl:w-[755px] "
         />
       </div>
       {children}
---
+++ src/page/aboutus/OurTeamSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+export default function OurTeamSection() {
+    const { t } = useTranslation("about");
+  return (
+    <div>
+      
+    </div>
+  )
+}
