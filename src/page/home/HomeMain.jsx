9f0510710e3a1ab1ca53fef5a4ea5563e5f525a7
Leaphea <ahpingpingshu@gmail.com>
2025-03-04 23:46:11 +0700

    hero-section

--- src/page/home/HomeMain.jsx
+++ src/page/home/HomeMain.jsx
@@ -2,8 +2,20 @@ import React from "react";
 import Hero from "./Hero";
 import shap from "../../../public/svg/1.svg";
 import ellipseHalf from "../../../public/svg/2.svg";
+import Container from "./Container";
+import Categories from "./Categories";
 
 const HomeMain = () => {
+  const Container = ({ children, className = "" }) => {
+    return (
+      <div
+        // className={`max-w-7xl mx-auto ${className}`}
+        className={`max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ${className}`}
+      >
+        {children}
+      </div>
+    );
+  };
   return (
     <div>
       <div className="relative">
@@ -20,8 +32,12 @@ const HomeMain = () => {
           className="absolute mt-[170px] w-[200px] left-0 z-[-1]"
         />
       </div>
+      <Container>
+        <Hero />
+        <br />
+        <Categories />
+      </Container>
 
-      <Hero />
     </div>
   );
 };
