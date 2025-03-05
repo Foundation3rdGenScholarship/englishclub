2e483e19aa8cde16357fc66a29a65fe7484e9248
Leaphea <ahpingpingshu@gmail.com>
2025-03-05 21:43:17 +0700

    add new section to homepage

--- src/page/home/Categories.jsx
+++ src/page/home/Categories.jsx
@@ -1,32 +1,102 @@
 import React from "react";
+import GlassCard from "../../components/card/GlassCard";
+import { RiHeadphoneLine, RiFileList3Line } from "react-icons/ri";
+import { FaRegFaceFrownOpen, FaArrowUpRightDots } from "react-icons/fa6";
+import { AiOutlinePieChart, AiOutlineRead } from "react-icons/ai";
 import { useTranslation } from "react-i18next";
-const Categories = () => {
-    const { t } = useTranslation("homepage");
-  return (
 
+const FeatureCard = ({ icon: Icon, title, description }) => (
+  <div className="h-full flex flex-col">
+    <div className="p-2 flex flex-col items-center text-center flex-grow">
+      <div
+        className={`w-12 h-12 rounded-full flex justify-center items-center mb-6`}
+      >
+        {/* Render the icon as a JSX component */}
+        {Icon && <Icon className="text-6xl text-black dark:text-white" />}
+      </div>
+      <h3 className="text-heading-4 font-bold text-primary-500 mb-4">
+        {title}
+      </h3>
+      <p className="text-des-2 text-gray-600 dark:text-white text-center">
+        {description}
+      </p>
+    </div>
+  </div>
+);
+
+const FeaturesGrid = () => {
+  const { t } = useTranslation("homepage");
+  const features = [
+    {
+      icon: RiHeadphoneLine,
+      title: t("ca-listening"),
+      description: t("ca-des-lis"),
+    },
+    {
+      icon: FaArrowUpRightDots,
+      title: t("ca-quiz"),
+      description: t("ca-des-quiz"),
+    },
+    {
+      icon: RiFileList3Line,
+      title: t("ca-exercises"),
+      description: t("ca-des-exercises"),
+    },
+    {
+      icon: AiOutlineRead,
+      title: t("ca-reading"),
+      description: t("ca-des-reading"),
+    },
+    {
+      icon: FaRegFaceFrownOpen,
+      title: t("ca-speaking"),
+      description: t("ca-des-speaking"),
+    },
+    {
+      icon: AiOutlinePieChart,
+      title: t("ca-vocabulary"),
+      description: t("ca-des-vocabulary"),
+    },
+  ];
+
+  return (
     <>
-      <div className="flex items-center justify-center ">
-        <div className="bg-white dark:bg-transparent rounded-lg shadow-md p-6 flex justify-around w-full">
+      {/* description  */}
+      <div>
+        <div className=" py-14 ">
           <div className="text-center">
-            <p className="text-heading-3 font-bold text-secondary-500">23</p>
-            <p className="text-lg text-black dark:text-white">{t("ca-student")}</p>
+            <span className="text-heading-3  font-bold leading-tight text-primary-500 dark:text-white ">
+              {t("des-title")}
+              <span className="text-secondary-500">
+                {" "}
+                {t("des-titleone")}
+              </span>{" "}
+              <span> {t("des-sub")}</span>
+            </span>
+
+            <p className="mt-4 text-des-2 leading-7 text-gray-500 sm:mt-8">
+              {t("des-subtwo")}
+            </p>
           </div>
-          <div className="text-center">
-            <p className="text-heading-3 font-bold text-secondary-500">219</p>
-            <p className="text-lg text-black dark:text-white">{t("ca-lesson")}</p>
-          </div>
-          <div className="text-center">
-            <p className="text-heading-3 font-bold text-secondary-500">56</p>
-            <p className="text-lg text-black dark:text-white">{t("ca-exercise")}</p>
-          </div>
-          <div className="text-center">
-            <p className="text-heading-3 font-bold text-secondary-500">40</p>
-            <p className="text-lg text-black dark:text-white">{t("ca-videos")}</p>
-          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-10 pb-6">
+          {features.map((feature, index) => (
+            <GlassCard
+              key={index}
+              className="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+            >
+              <FeatureCard
+                icon={feature.icon}
+                title={feature.title}
+                description={feature.description}
+              />
+            </GlassCard>
+          ))}
         </div>
       </div>
     </>
   );
 };
 
-export default Categories;
+export default FeaturesGrid;
