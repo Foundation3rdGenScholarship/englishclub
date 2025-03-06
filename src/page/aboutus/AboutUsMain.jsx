2f424534cadf7130c22f5c0184893368b3a7bcfc
Eric-Va01 <ericva014@gmail.com>
2025-03-06 10:18:15 +0700

    update : aboutus

--- src/page/aboutus/AboutUsMain.jsx
+++ src/page/aboutus/AboutUsMain.jsx
@@ -21,11 +21,11 @@ const AboutUsMain = () => {
   }, []);
   const { t } = useTranslation("about");
   return (
-    <main className="max-w-screen-xl mx-auto bg-">
-      <section className="px-4 md:px-8 xl:px-16 py-10">
+    <main className=" mx-auto max-w-full overflow-hidden">
+      <section className="px-4 md:px-8 xl:px-16 py-10 ">
         <div className="flex justify-center">
           <AboutUsShape className="max-w-[1205px] flex justify-center relative -z-10">
-            <GlassCard className="relative top-20 z-20 w-full  max-w-[1000px] md:max-w-[1100px] xl:max-w-[1205px] px-4 md:px-8 xl:px-12 py-6 md:py-8 xl:py-10">
+            <GlassCard className="relative top-20 z-20 w-full  max-w-[1000px] md:max-w-[1100px] xl:max-w-[1205px] px-4 md:px-8 xl:px-12 py-6 md:py-8 xl:py-10 sm:border-stone-200 md:border-stone-200 xl:border-white">
               <div className="text-center">
                 <h2 className="text-2xl md:text-3xl xl:text-4xl font-bold text-black dark:text-white mt-4">
                   {t("title")}
@@ -41,35 +41,36 @@ const AboutUsMain = () => {
           </AboutUsShape>
         </div>
       </section>
-
-      <div className="mt-40 md:mt-96">
-        <div>
-          <p className="text-center text-primary-950 dark:text-primary-100 font-bold text-heading-3">
-            {t("mentor")}
-          </p>
-        </div>
-        <div>
-          <MentorCard></MentorCard>
-        </div>
-      </div>
-      <Bubbles className="mt-36">
-        <div className="relative z-10 flex flex-col gap-8 justify-center items-center h-screen">
-          <div data-aos="fade-up" data-aos-anchor-placement="top-bottom">
-            <MissionCard />
+      <section className="container max-w-screen-xl mx-auto">
+        <div className="mt-40 md:mt-96">
+          <div>
+            <p className="text-center text-primary-950 dark:text-primary-100 font-bold text-heading-3">
+              {t("mentor")}
+            </p>
           </div>
           <div>
-            <VissionCard />
+            <MentorCard></MentorCard>
           </div>
         </div>
-      </Bubbles>
-      <div className="mt-36">
-        <p className="text-center text-primary-950 dark:text-primary-100  font-bold text-heading-3">
-          {t("teammember")}
-        </p>
-      </div>
-      <div className="mb-12 mt-6">
-        <MemberCard />
-      </div>
+        <Bubbles className="mt-36">
+          <div className="relative z-10 flex flex-col gap-8 justify-center items-center h-screen">
+            <div data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+              <MissionCard />
+            </div>
+            <div>
+              <VissionCard />
+            </div>
+          </div>
+        </Bubbles>
+        <div className="mt-36">
+          <p className="text-center text-primary-950 dark:text-primary-100  font-bold text-heading-3">
+            {t("teammember")}
+          </p>
+        </div>
+        <div className="mb-12 mt-6">
+          <MemberCard />
+        </div>
+      </section>
     </main>
   );
 };
--- src/page/aboutus/AboutUsShape.jsx
+++ src/page/aboutus/AboutUsShape.jsx
@@ -37,9 +37,9 @@ export default function AboutUsShape({ children, className = "" }) {
           className="w-[400px] fill-primary-100 sm:w-[500px] md:w-[600px] lg:w-[700px] xl:w-[755px]"
         />
       </div>
-      <div className="absolute -right-48 -top-6 -z-10 hidden md:block">
+      <div className="absolute -top-6 -z-10 hidden lg:block  lg:-right-8 xl:-right-48">
         <img
-        className="w-auto h-[730px]"
+        className="sm:h-[300px] md:h-[550px] lg:h-[600px] xl:h-[700px]"
         src={rightshape} alt="" />
       </div>
       {children}
