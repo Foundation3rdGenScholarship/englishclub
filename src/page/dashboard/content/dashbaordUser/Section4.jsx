172da35468b743215338f75e9c48fedbc9d37a51
Tong Bora <tongbora.official@gamil.com>
2025-03-07 15:20:35 +0700

    update all everything, last version

--- src/page/dashboard/content/dashbaordUser/Section4.jsx
+++ src/page/dashboard/content/dashbaordUser/Section4.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import GlassCard from "../../../../components/card/GlassCard";
+import ButtonNavigate from "../../../../components/button/ButtonNavigate";
 export default function Section4() {
   const { t } = useTranslation("dashboard-overview");
   return (
     <>
-      <div className="flex flex-col gap-8 justify-center py-16">
+      <div className="flex flex-col gap-8 justify-center xl:pb-16 lg:pb-14 md:pb-12 sm:pb-10 pb-8">
         <h2 className="dark:text-text-des-dark-mode text-primary-500 lg:text-heading-2 md:text-heading-3 text-heading-4 text-center">
           {t("startWith")}
           <span className="text-accents-color">{t("fluentflow")}</span>
         </h2>
-        <div className="flex lg:flex-nowrap flex-wrap lg:justify-between md:justify-between justify-center lg:gap-5 gap-4">
+        <div className="flex lg:flex-nowrap flex-wrap lg:justify-between md:justify-center justify-center lg:gap-5 gap-4">
           <GlassCard className="flex flex-col gap-6 justify-center items-center lg:w-1/2 md:w-3/4 w-full p-4">
             <h4 className="dark:text-accents-color text-primary-950 lg:text-heading-4 md:text-heading-5 sm:text-heading-6 text-heading-6 font-semibold text-center">
               {t("exercises")}
@@ -18,9 +19,7 @@ export default function Section4() {
             <p className="dark:text-white text-primary-800  lg:text-des-2 md:text-des-3 sm:text-des-4 text-des-5 text-center">
               {t("desExercises")}
             </p>
-            <button className="flex bg-secondary-500 px-[25px] py-[11px] text-heading-6  text-white font-semibold rounded-[25px]">
-              Start Learning
-            </button>
+            <ButtonNavigate text={t("start-learning")} link="/" onClick={""} />
           </GlassCard>
           <GlassCard className="flex flex-col gap-6 justify-center items-center lg:w-1/2 md:w-3/4 w-full p-4">
             <h4 className="dark:text-accents-color text-primary-950 lg:text-heading-4 md:text-heading-5 sm:text-heading-6 text-heading-6 font-semibold text-center">
@@ -29,9 +28,7 @@ export default function Section4() {
             <p className="dark:text-white text-primary-800  lg:text-des-2 md:text-des-3 sm:text-des-4 text-des-5 text-center">
               {t("desMoreVideos")}
             </p>
-            <button className="flex bg-secondary-500 px-[25px] py-[11px] text-heading-6  text-white font-semibold rounded-[25px]">
-              Start Learning
-            </button>
+            <ButtonNavigate text={t("start-learning")} link="/" onClick={""} />
           </GlassCard>
         </div>
       </div>
