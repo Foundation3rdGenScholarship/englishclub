98e73bb32e130f080da787f40551d2c10ee2e93a
Tong Bora <tongbora.official@gamil.com>
2025-03-07 13:49:13 +0700

    add error and not found page

--- src/page/err/ServerErrorPage.jsx
+++ src/page/err/ServerErrorPage.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-
+import { useTranslation } from "react-i18next";
+import { MdError } from "react-icons/md";
 const ServerErrorPage = () => {
+  const { t } = useTranslation("error");
   return (
     <div className="max-w-screen-xl sm:ml-64 flex flex-col items-center justify-center min-h-screen bg-bg-light-mode dark:bg-bg-dark-mode text-text-des-light-mode dark:text-text-des-dark-mode">
       <div className="text-center max-w-md">
-        <div className="mb-6">
-          <div className="mx-auto h-16 w-16 rounded-full bg-bg-light-mode dark:bg-bg-dark-mode flex items-center justify-center border-2 dark:border-text-des-dark-mode border-text-des-light-mode">
-            <div className=" text-3xl font-bold ">!</div>
+        <div className="mx-auto mb-6 bg-bg-light-mode dark:bg-bg-dark-mode flex items-center justify-center dark:border-text-des-dark-mode border-text-des-light-mode">
+          <div className="h-screen flex flex-col justify-center items-center">
+            <MdError className="text-8xl pb-2" />
+            <p className="text-4xl font-medium text-text-des-light-mode dark:text-text-des-dark-mode">
+              Internal Server Error
+            </p>
+            <p className="text-xl text-text-des-light-mode dark:text-text-des-dark-mode mt-4">
+              {t("apologize")}
+            </p>
           </div>
         </div>
-
-        <h1 className="text-2xl font-medium mb-4">Server error</h1>
-
-        <p className="text-base">
-          An error occurred in the application and your page could not be
-          served. If you are the application owner, check your logs for details.
-        </p>
       </div>
     </div>
   );
