310702f72a7362eeab8cd0835acccf03ef78084e
RML-Cambo <ltomusic69@gmail.com>
2025-03-10 09:35:02 +0700

    submit

--- src/page/dashboard/content/grammars/MoreDoc.jsx
+++ src/page/dashboard/content/grammars/MoreDoc.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import UserAnswers from "../../../test/UserAnswers";
 
 const MoreDoc = () => {
   const { t } = useTranslation("dashboard");
   return (
     <div className="p-4 sm:ml-64  mt-[88px] h-[3000px]">
-      <h1 className="dark:text-white text-black">
+      {/* <h1 className="dark:text-white text-black">
         This Content of More Document
-      </h1>
+      </h1> */}
+      <UserAnswers />
     </div>
   );
 };
