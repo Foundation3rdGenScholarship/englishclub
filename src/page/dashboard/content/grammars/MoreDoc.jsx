d67afb48b991e46e022903947c34e95c219087f5
Oudom Phoem <oudomphoem@gmail.com>
2025-03-11 14:18:39 +0700

    fix: importing Pagination

--- src/page/dashboard/content/grammars/MoreDoc.jsx
+++ src/page/dashboard/content/grammars/MoreDoc.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useState, useEffect } from "react";
-import Pagination from "../../../test/pagination";
+import Pagination from "../../../test/Pagination";
 // import UserAnswers from "../../../test/UserAnswers";
 
 const MoreDoc = () => {
