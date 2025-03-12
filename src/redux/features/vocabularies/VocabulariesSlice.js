a857b5b093fd847ff66acdfcbd21278b8e991ae9
oudomm <oudomphoem@gmail.com>
2025-03-12 10:39:16 +0700

    fix 404 vercel 5

--- src/redux/features/vocabularies/VocabulariesSlice.js
+++ src/redux/features/vocabularies/VocabulariesSlice.js
@@ -3,7 +3,7 @@ import { apiSlice } from "../../../api/apiSlice";
 export const vocabulariesSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     allVocabularies: builder.query({
-      query: () => `/vocabularies/`,
+      query: () => `/vocabularies`,
       method: "GET",
       transformErrorResponseK: (response) => response.payloard,
     }),
