8465d62015aa9d3940f35576e4bd5b501a5f034f
oudomm <oudomphoem@gmail.com>
2025-03-13 09:50:25 +0700

    fix vercel 404 and CORS

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensure this is set correctly
-  build: {
-    outDir: "public",
+  server: {
+    host: "0.0.0.0",
+    proxy: {
+      "/api": {
+        target: "https://english-club.istad.co",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
