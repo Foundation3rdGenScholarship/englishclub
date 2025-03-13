582eab00686123c4ae6d5d476e1782719d49abab
oudomm <oudomphoem@gmail.com>
2025-03-13 10:42:35 +0700

    update: updated vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    proxy: {
-      "/api": {
-        target: "https://english-club.istad.co",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
