7fea885690663cc0d9f26e9f8e5e1a7e03a5fa82
Tong Bora <tongbora.official@gamil.com>
2025-03-14 23:38:18 +0700

    change button

--- src/page/home/ContentSectionCard.jsx
+++ src/page/home/ContentSectionCard.jsx
@@ -91,7 +91,7 @@ export default function ContentSectionCard() {
                 <ButtonNavigate
                   text={t("start Learning")}
                   link={item.link}
-                  addMoreStyle="xl:text-heading-4 lg:text-heading-5 md:text-heading-6 text-[14px] "
+                  addMoreStyle=""
                   
                 />
               </div>
