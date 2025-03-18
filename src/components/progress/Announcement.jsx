f69196918b545cbe2e2640e4653db15678af1e90
RML-Cambo <ltomusic69@gmail.com>
2025-03-18 17:29:37 +0700

    contact

--- src/components/progress/Announcement.jsx
+++ src/components/progress/Announcement.jsx
@@ -5,7 +5,7 @@ import { IoMdClose } from "react-icons/io";
 const Announcement = () => {
   const { t } = useTranslation("announcement");
   const storageKey = "announcement_closed_date";
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
@@ -40,12 +40,12 @@ const Announcement = () => {
 
   return (
     <div
-      className={`bg-primary-950 px-4 py-3 text-white sticky top-0 transition-transform duration-300 ${
+      className={`bg-primary-950 px-4 py-3 text-white sticky top-0 z-50 transition-transform duration-300 ${
         isScrolled ? "-translate-y-full" : "translate-y-0"
       }`}
     >
       <div className="max-w-7xl flex items-center m-auto">
-        <div className="flex-grow text-center">
+        <div className="flex-grow md:text-center">
           <p className="text-sm font-medium">
             {t("title")}
             <a href="/skills" className="inline-block underline ml-1">
@@ -57,7 +57,7 @@ const Announcement = () => {
         <button
           onClick={handleClose}
           aria-label="Close"
-          className="shrink-0 rounded-lg bg-black/10 p-1 transition hover:bg-black/20 xl:mr-4"
+          className="shrink-0 rounded-lg bg-black/10 p-1 transition hover:bg-black/20 xl:mr-5"
         >
           <IoMdClose />
         </button>
